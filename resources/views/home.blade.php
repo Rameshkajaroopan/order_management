61cea71f3024c3d7a5ead63299135348e51413f8
Thushapan <thushapan1996@gmail.com>
2022-01-10 21:03:56 +0530

    image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -281,7 +281,7 @@
 
   <section class="content ">
   <div class="card-header border-transparent">
-        <h3 class="card-title">Tody's Payment Summary</h3>
+        <h3 class="card-title">Today's Payment Summary</h3>
 
         <div class="card-tools">
           <button type="button" class="btn btn-tool" data-card-widget="collapse">
