198ea284e5d7a2b9f61ada0a852b547b5ed0bf53
Thushapan <thushapan1996@gmail.com>
2022-01-10 21:27:58 +0530

    image

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="{{ asset('storage/Logo.png')}}" 
+        <img src="{{ asset('logo.png')}}" 
              alt="AdminLTE Logo"
              class="brand-image  elevation-1">
         <span class="brand-text font-weight-light">Order Management</span>
